96ee146831c5be2284b1156f627a992097868943
sprinzs123 <sprinzs351@gmail.com>
2020-12-16 17:33:54 -0500

    able to draw recursevly

--- script.js
+++ script.js
@@ -1,238 +1,218 @@
 // nodes are stored in x y format with index starting at 0
 
-
-let width = 50
-let height = 20
-
+let width = 50;
+let height = 20;
 
 // creates grid using html tables
-function createGrid(){   
-    let board = document.getElementById('board')
+function createGrid() {
+    let board = document.getElementById("board");
     let tableHTML = "";
     for (let r = 0; r < height; r++) {
-        let currentArrayRow = []
-        let currentHTMLRow = `<tr id="row ${r}">` 
-        for (let c = 0; c < width; c ++){
-            let newNodeId = `${r}-${c}`
+        let currentArrayRow = [];
+        let currentHTMLRow = `<tr id="row ${r}">`;
+        for (let c = 0; c < width; c++) {
+            let newNodeId = `${r}-${c}`;
             let newNodeClass = "unvisited";
-            currentHTMLRow += `<th id="${newNodeId}" class="${newNodeClass}"></th>`;            
-        };
-    currentHTMLRow += `</tr>`
-    tableHTML += currentHTMLRow    
-    };
-    board.innerHTML = tableHTML    
-};
-createGrid()
-
+            currentHTMLRow += `<th id="${newNodeId}" class="${newNodeClass}"></th>`;
+        }
+        currentHTMLRow += `</tr>`;
+        tableHTML += currentHTMLRow;
+    }
+    board.innerHTML = tableHTML;
+}
+createGrid();
 
 // checks if start or end node already plotted
 // input class looking for in table and returns true or false if found or not
-function checkTableNode(classCheck){
-    let grid = document.querySelector('.grid').innerHTML   
-    if(grid.includes(classCheck)){
-        return true
+function checkTableNode(classCheck) {
+    let grid = document.querySelector(".grid").innerHTML;
+    if (grid.includes(classCheck)) {
+        return true;
+    } else {
+        return false;
     }
-    else{
-        return false
-    }   
 }
-
 
 // make starter and end points point
 // class is passed to what node need to do
 // do a check if this node exists in table already
-function makeStartNodes(newNode){
-    let allUnvisited = document.querySelectorAll('.unvisited')
-    allUnvisited.forEach(point => {
-        point.addEventListener('click', function(){
-            if(checkTableNode(newNode)){
-                console.log(checkTableNode(newNode));                
+function makeStartNodes(newNode) {
+    let allUnvisited = document.querySelectorAll(".unvisited");
+    allUnvisited.forEach((point) => {
+        point.addEventListener("click", function () {
+            if (checkTableNode(newNode)) {
+                console.log(checkTableNode(newNode));
+            } else {
+                point.classList = newNode;
             }
-            else{
-                point.classList = newNode
-            }
-        })
+        });
     });
 }
 // makeStartNodes('end-node')
 // makeStartNodes('start-node')
 
-
 // make walls
-function wallGenerator(){
-    let allUnvisited = document.querySelectorAll('.unvisited')
-    allUnvisited.forEach(point => {
-        point.addEventListener('mouseover', function(){
-            if(canDrag == true){
-                point.classList = 'wall-node'
-
+function wallGenerator() {
+    let allUnvisited = document.querySelectorAll(".unvisited");
+    allUnvisited.forEach((point) => {
+        point.addEventListener("mouseover", function () {
+            if (canDrag == true) {
+                point.classList = "wall-node";
             }
-        });   
+        });
     });
 }
-wallGenerator()
+wallGenerator();
 
-
-let canDrag = false
+let canDrag = false;
 
 // check if mouse is down
-function isMouseDown(){
-    let allUnvisited = document.querySelectorAll('.unvisited')
-    allUnvisited.forEach(point => {
-        point.addEventListener('mousedown', function(){
-            canDrag = true
-        });   
-    });    
+function isMouseDown() {
+    let allUnvisited = document.querySelectorAll(".unvisited");
+    allUnvisited.forEach((point) => {
+        point.addEventListener("mousedown", function () {
+            canDrag = true;
+        });
+    });
 }
-isMouseDown()
+isMouseDown();
 
 // check if mouse is up so no longer able to draw borders
-function isMouseReleased(){
-    let allUnvisited = document.querySelectorAll('.unvisited')
-    allUnvisited.forEach(point => {
-        point.addEventListener('mouseup', function(){
-            canDrag = false
-        });   
-    });    
+function isMouseReleased() {
+    let allUnvisited = document.querySelectorAll(".unvisited");
+    allUnvisited.forEach((point) => {
+        point.addEventListener("mouseup", function () {
+            canDrag = false;
+        });
+    });
 }
-isMouseDown()
-isMouseReleased()
-
-
+isMouseDown();
+isMouseReleased();
 
 // hard coded start and end points
-function makeNodes(){
-    let start = document.getElementById('9-39')
-    let end = document.getElementById('9-10')
-    start.classList = 'start-node'
-    end.classList = 'end-node'
+function makeNodes() {
+    let start = document.getElementById("9-39");
+    let end = document.getElementById("9-10");
+    start.classList = "start-node";
+    end.classList = "end-node";
 }
-makeNodes()
+makeNodes();
 
-let visited = []
-let frontier = []
-let solution = {}
+let visited = [];
+let frontier = [];
+let solution = {};
 
-let startNode = document.querySelector('.start-node').id
-let endNode = document.querySelector('.end-node').id
-let testPoint = []
-testPoint.push(startNode)
-
+let startNode = document.querySelector(".start-node").id;
+let endNode = document.querySelector(".end-node").id;
+let testPoint = [];
+testPoint.push(startNode);
 
 // github https://github.com/tonypdavis/BFS-Maze-Solver/blob/master/BFS_v2.py
-function search(x, y){
-    frontier.push((x, y))
-    solution.put(x, y)
-    while(frontier.length > 0){
-            }
+function search(x, y) {
+    frontier.push((x, y));
+    solution.put(x, y);
+    while (frontier.length > 0) { }
 }
 
-
-let nodesList = []
-let foundEnd = false
+let nodesList = [];
+let foundEnd = false;
 
 // check if coordinates for future nodes are valid/are not out of bounds
 // in and out list of nodes
-function checkByCoordinates(nodeList){
-    let approved = []
-    nodeList.forEach(node =>{
-        let coordinates = node.split('-')    
-        let row = parseInt(coordinates[0])
-        let column = parseInt(coordinates[1])
-        if(column >= 0 && column < width && row >=0 && row < height){           
-            approved.push(node)
+function checkByCoordinates(nodeList) {
+    let approved = [];
+    nodeList.forEach((node) => {
+        let coordinates = node.split("-");
+        let row = parseInt(coordinates[0]);
+        let column = parseInt(coordinates[1]);
+        if (column >= 0 && column < width && row >= 0 && row < height) {
+            approved.push(node);
         }
-    })    
-    return approved
+    });
+    return approved;
 }
+
 
 
 
 
 // check if adjacent node is unvisited node
-function checkNodesType(nodeList){
-    let approved = []
-    nodeList.forEach(nodeID =>{
-        oneNode = document.getElementById(nodeID)
-        if(oneNode.classList == 'unvisited'){
-            approved.push(nodeID)
+function checkNodesType(nodeList) {
+    let approved = [];
+    nodeList.forEach((nodeID) => {
+        oneNode = document.getElementById(nodeID);
+        if (oneNode.classList == "unvisited") {
+            approved.push(nodeID);
         }
-        if(oneNode.classList == 'end-node'){
-            approved.push(nodeID)
-            foundEnd = true            
+        if (oneNode.classList == "end-node") {
+            approved.push(nodeID);
+            foundEnd = true;
         }
-    })
-    return approved
+    });
+    return approved;
 }
+
+
+function inertStartNode() {
+    let startNode = document.querySelector(".start-node").id;
+    nodesList.push(startNode);
+}
+inertStartNode();
 
 
 // main function to get all valid node
-function inertStartNode(){
-    let startNode = document.querySelector('.start-node').id
-    nodesList.push(startNode)
-    let count = 0
-    while(count < 10000000){
-        let firstNode = nodesList[0]
-        let nodeObject = document.getElementById(firstNode)        
-        nodeObject.classList = 'visited-node'
-        let validNeighbors = newNodes(firstNode)
-        validNeighbors.forEach( oneNode => {
-            nodesList.push(oneNode)        
-        })
-        nodesList.shift()
-        count ++
+function DrawNodes() {
+    if (foundEnd == false) {
+        setTimeout(() => {
+            console.log('loop')
+            let firstNode = nodesList[0];
+            let nodeObject = document.getElementById(firstNode);
+            nodeObject.classList = "visited-node";
+            let validNeighbors = newNodes(firstNode);
+            validNeighbors.forEach((oneNode) => {
+                nodesList.push(oneNode);
+            });
+            nodesList.shift();
+            DrawNodes()
+        }, 30);
+
     }
+
+
 }
-// inertStartNode()
 
-
-
+// DrawNodes()
 
 // input is id of a sting
 // get new/adjacent coordinates of a node
 // node coordinates input, list of new  valid coordinates output as list
-function newNodes(nodeGrid){
-    let coordinates = nodeGrid.split('-')
-    let newCoordinates = []
-    let row = parseInt(coordinates[0])
-    let column = parseInt(coordinates[1])
-    newCoordinates.push((row + '-' + (column -1)))
-    newCoordinates.push((row + '-' + (column +1)))
-    newCoordinates.push(((row +1) + '-' + column))
-    newCoordinates.push(((row -1)+ '-' + column))
-    newCoordinates = checkByCoordinates(newCoordinates)
-    return checkNodesType(newCoordinates)
+function newNodes(nodeGrid) {
+    let coordinates = nodeGrid.split("-");
+    let newCoordinates = [];
+    let row = parseInt(coordinates[0]);
+    let column = parseInt(coordinates[1]);
+    newCoordinates.push(row + "-" + (column - 1));
+    newCoordinates.push(row + "-" + (column + 1));
+    newCoordinates.push(row + 1 + "-" + column);
+    newCoordinates.push(row - 1 + "-" + column);
+    newCoordinates = checkByCoordinates(newCoordinates);
+    return checkNodesType(newCoordinates);
 }
 
 
-// trying with pushing and upshifting array
-function makeList(){
-    let validNeighbors = newNodes(nodesList[0])
-    validNeighbors.forEach( oneNode => {
-        nodesList.push(oneNode)
-    })
-    
+
+
+// working version of setTimeout
+function test(count) {
+    if (count < 5) {
+        console.log(count)
+        setTimeout(() => {
+            count++
+            test(count)
+        }, 3000);
+    }
 }
 
-// makeList()
 
+// test(1)
 
-// recursive function to show new nodes on graphs
-// everything prototype o far and hard coded
-function showNewNodes(nodeId){
-    let nodeList = newNodes(nodeId)
-    let found = false
-    nodeList.forEach(oneNode => {
-        let nodeClass = document.getElementById(oneNode).classList
-        if( nodeClass[0] == 'unvisited' && found == false){
-            let nodeObject = document.getElementById(oneNode)
-            nodeObject.classList = 'visited-node'
-            showNewNodes(oneNode)            
-        }
-        if( nodeClass[0] == 'end-node'){
-            found = true
-        }
-    })
-}
-
-// showNewNodes('9-10')
